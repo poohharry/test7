b3e4d122efd7efa10b311115020b1d3c0c6a4091
poohharry <81311834+poohharry@users.noreply.github.com>
2023-02-24 17:08:39 +0900

    Add files via upload

---
+++ init.sql
@@ -0,0 +1,17 @@
+CREATE USER 'scott'@'localhost' IDENTIFIED BY 'tiger';
+CREATE USER 'scott'@'%' IDENTIFIED BY 'tiger';
+GRANT ALL PRIVILEGES ON *.* TO 'scott'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'scott'@'%';
+FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS `dept` (
+  `DEPTNO` int(11) DEFAULT NULL,
+  `DNAME` varchar(14) DEFAULT NULL,
+  `LOC` varchar(13) DEFAULT NULL
+);
+
+INSERT INTO `dept` (`DEPTNO`, `DNAME`, `LOC`) VALUES
+(10, 'ACCOUNTING', 'NEW YORK'),
+(20, 'RESEARCH', 'DALLAS'),
+(30, 'SALES', 'CHICAGO'),
+(40, 'OPERATIONS', 'BOSTON');
